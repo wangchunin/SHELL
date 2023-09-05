0d145472a3de7df4680eb9fe1f6bec4eb7359b0b
wangchunin <1104436919@qq.com>
2023-09-05 12:09:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -40,34 +40,34 @@ fs.readdir(directory, (err, files) => {
     }
     console.log('目录列表已生成');
   });
-});
 
-// 为每个文件创建对应的路由
-files.forEach((file) => {
-  const filePath = path.join(directory, file);
+  // 为每个文件创建对应的路由
+  files.forEach((file) => {
+    const filePath = path.join(directory, file);
 
-  // 创建路由处理函数
-  const handleFile = (request, response) => {
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-      if (err) {
-        console.error(err);
-        response.statusCode = 500;
-        response.end('Internal Server Error');
-        return;
+    // 创建路由处理函数
+    const handleFile = (request, response) => {
+      fs.readFile(filePath, 'utf-8', (err, data) => {
+        if (err) {
+          console.error(err);
+          response.statusCode = 500;
+          response.end('Internal Server Error');
+          return;
+        }
+
+        response.setHeader('Content-Type', 'text/plain');
+        response.end(data);
+      });
+    };
+
+    // 注册路由
+    addEventListener('fetch', (event) => {
+      const request = event.request;
+      const url = new URL(request.url);
+
+      if (url.pathname === `/${file}`) {
+        event.respondWith(new Response(handleFile(request, response)));
       }
-
-      response.setHeader('Content-Type', 'text/plain');
-      response.end(data);
     });
-  };
-
-  // 注册路由
-  addEventListener('fetch', (event) => {
-    const request = event.request;
-    const url = new URL(request.url);
-
-    if (url.pathname === `/${file}`) {
-      event.respondWith(new Response(handleFile(request, response)));
-    }
   });
 });
