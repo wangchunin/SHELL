0d40e27f87bdec64c91c3858c96564511f9498a3
wangchunin <1104436919@qq.com>
2023-09-05 13:44:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 // index.js
 
-const express = require('express');
-const fetch = require('node-fetch');
+const apiUrl = 'https://api.github.com/repos/wangchunin/SHELL/contents';
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.get('*', async (req, res) => {
-  const path = req.path;
-
-  // 获取 GitHub 仓库的目录和文件
-  const response = await fetch(`https://api.github.com/repos/wangchunin/SHELL/contents${path}`, {
+async function fetchDirectory(path) {
+  const response = await fetch(apiUrl + path, {
     headers: {
       'User-Agent': 'Mozilla/5.0 (compatible; Cloudflare-Worker/1.0)',
       'Accept': 'application/vnd.github.v3+json'
@@ -18,37 +11,58 @@ app.get('*', async (req, res) => {
   });
 
   if (!response.ok) {
-    return res.status(404).send('Not found');
+    throw new Error('Failed to fetch directory');
   }
 
   const data = await response.json();
+  return data;
+}
 
-  if (Array.isArray(data)) {
-    // 如果返回的是目录列表，则生成目录的 HTML 页面
-    const html = generateDirectoryPage(data, path);
-    res.send(html);
-  } else {
-    // 如果返回的是文件内容，则直接跳转到 GitHub 的文件显示页面
-    const fileUrl = data.html_url;
-    res.redirect(fileUrl);
-  }
-});
+function generateDirectoryPage(data, path) {
+  const container = document.getElementById('container');
+  container.innerHTML = '';
 
-function generateDirectoryPage(data, currentPath) {
-  // 生成目录的 HTML 页面
-  let html = '<h1>Directory</h1>';
-  html += '<ul>';
+  const title = document.createElement('h1');
+  title.textContent = 'Directory';
+  container.appendChild(title);
+
+  const ul = document.createElement('ul');
+  container.appendChild(ul);
+
   data.forEach(item => {
+    const li = document.createElement('li');
+    ul.appendChild(li);
+
     const itemName = item.name;
     const itemPath = item.path;
     const isDirectory = item.type === 'dir';
-    const link = isDirectory ? `<a href="${itemPath}">${itemName}</a>` : `<a href="${item.html_url}" target="_blank">${itemName}</a>`;
-    html += `<li>${link}</li>`;
+
+    if (isDirectory) {
+      const link = document.createElement('a');
+      link.textContent = itemName;
+      link.href = itemPath;
+      link.addEventListener('click', async (event) => {
+        event.preventDefault();
+        await showDirectory(itemPath);
+      });
+      li.appendChild(link);
+    } else {
+      const link = document.createElement('a');
+      link.textContent = itemName;
+      link.href = itemPath;
+      link.target = '_blank';
+      li.appendChild(link);
+    }
   });
-  html += '</ul>';
-  return html;
 }
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+async function showDirectory(path) {
+  try {
+    const data = await fetchDirectory(path);
+    generateDirectoryPage(data, path);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+showDirectory('/');
