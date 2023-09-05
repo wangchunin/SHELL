0092b9ae3952667944afca1fe1c68a9262143ee4
wangchunin <1104436919@qq.com>
2023-09-05 12:33:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-const fs = require('fs');
-const path = require('path');
-
-const rootDirectory = __dirname; // 根目录路径
-
 // 递归遍历目录结构并生成文件列表
 function generateFileList(directory) {
   const files = fs.readdirSync(directory);
@@ -23,64 +18,49 @@ function generateFileList(directory) {
   return fileList;
 }
 
-// 生成文件列表
-const files = generateFileList(rootDirectory);
+addEventListener('fetch', (event) => {
+  const { request } = event;
+  const { pathname } = new URL(request.url);
 
-// 生成目录列表的 HTML
-const html = `
-  <!DOCTYPE html>
-  <html>
-  <head>
-    <title>文件列表</title>
-    <style>
-      pre {
-        white-space: pre-wrap;
-        font-family: monospace;
-      }
-    </style>
-  </head>
-  <body>
-    <h1>文件列表</h1>
-    <ul>
-      ${files.map((file) => `<li><a href="${file}">${file}</a></li>`).join('\n')}
-    </ul>
-  </body>
-  </html>
-`;
+  if (pathname === '/') {
+    // 生成文件列表
+    const files = generateFileList(__dirname);
 
-// 将生成的 HTML 写入 index.html 文件
-fs.writeFile('index.html', html, (err) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log('目录列表已生成');
-});
+    // 生成目录列表的 HTML
+    const html = `
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <title>文件列表</title>
+        <style>
+          pre {
+            white-space: pre-wrap;
+            font-family: monospace;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>文件列表</h1>
+        <ul>
+          ${files.map((file) => `<li><a href="${file}">${file}</a></li>`).join('\n')}
+        </ul>
+      </body>
+      </html>
+    `;
 
-// 为每个文件创建对应的路由
-files.forEach((file) => {
-  // 创建路由处理函数
-  const handleFile = (request, response) => {
-    fs.readFile(file, 'utf-8', (err, data) => {
+    event.respondWith(new Response(html, { headers: { 'Content-Type': 'text/html' } }));
+  } else {
+    // 处理文件请求
+    const filePath = path.join(__dirname, pathname.substr(1));
+
+    fs.readFile(filePath, 'utf-8', (err, data) => {
       if (err) {
         console.error(err);
-        response.statusCode = 500;
-        response.end('Internal Server Error');
+        event.respondWith(new Response('Internal Server Error', { status: 500 }));
         return;
       }
 
-      response.setHeader('Content-Type', 'text/plain');
-      response.end(data);
+      event.respondWith(new Response(data, { headers: { 'Content-Type': 'text/plain' } }));
     });
-  };
-
-  // 注册路由
-  addEventListener('fetch', (event) => {
-    const request = event.request;
-    const url = new URL(request.url);
-
-    if (url.pathname === `/${file}`) {
-      event.respondWith(new Response(handleFile(request, response)));
-    }
-  });
+  }
 });
