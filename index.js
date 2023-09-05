f1c401771eef796d9d39f244f17576ce3dc0aa4a
wangchunin <1104436919@qq.com>
2023-09-05 12:13:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -60,27 +60,16 @@ fs.writeFile('index.html', html, (err) => {
 // 为每个文件创建对应的路由
 files.forEach((file) => {
   // 创建路由处理函数
-  const handleFile = (request, response) => {
-    fs.readFile(file, 'utf-8', (err, data) => {
-      if (err) {
-        console.error(err);
-        response.statusCode = 500;
-        response.end('Internal Server Error');
-        return;
-      }
+  const handleFile = async (request) => {
+    const data = await fs.promises.readFile(file, 'utf-8');
 
-      response.setHeader('Content-Type', 'text/plain');
-      response.end(data);
+    return new Response(data, {
+      headers: {
+        'Content-Type': 'text/plain',
+      },
     });
   };
 
   // 注册路由
-  addEventListener('fetch', (event) => {
-    const request = event.request;
-    const url = new URL(request.url);
-
-    if (url.pathname === `/${file}`) {
-      event.respondWith(new Response(handleFile(request, response)));
-    }
-  });
+  addRoute(`/${file}`, handleFile);
 });
