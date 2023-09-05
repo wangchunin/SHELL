a4da1dcbd6ed9008f127cce0aa312b9f1e340c58
wangchunin <1104436919@qq.com>
2023-09-05 12:08:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const directory = './'; // 目录路径
+const directory = path.join(__dirname, 'files'); // 文本文件所在的目录路径
 
 // 读取目录中的文件列表
 fs.readdir(directory, (err, files) => {
@@ -16,6 +16,12 @@ fs.readdir(directory, (err, files) => {
     <html>
     <head>
       <title>文件列表</title>
+      <style>
+        pre {
+          white-space: pre-wrap;
+          font-family: monospace;
+        }
+      </style>
     </head>
     <body>
       <h1>文件列表</h1>
@@ -33,5 +39,35 @@ fs.readdir(directory, (err, files) => {
       return;
     }
     console.log('目录列表已生成');
+  });
+});
+
+// 为每个文件创建对应的路由
+files.forEach((file) => {
+  const filePath = path.join(directory, file);
+
+  // 创建路由处理函数
+  const handleFile = (request, response) => {
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+      if (err) {
+        console.error(err);
+        response.statusCode = 500;
+        response.end('Internal Server Error');
+        return;
+      }
+
+      response.setHeader('Content-Type', 'text/plain');
+      response.end(data);
+    });
+  };
+
+  // 注册路由
+  addEventListener('fetch', (event) => {
+    const request = event.request;
+    const url = new URL(request.url);
+
+    if (url.pathname === `/${file}`) {
+      event.respondWith(new Response(handleFile(request, response)));
+    }
   });
 });
