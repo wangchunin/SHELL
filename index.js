d4e5df29eb395f81a0d5d9e00bbc62e8e5fb4344
wangchunin <1104436919@qq.com>
2023-09-05 13:36:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// index.js
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
@@ -6,41 +8,42 @@ async function handleRequest(request) {
   const url = new URL(request.url)
   const path = url.pathname
 
-  // 如果请求的路径不是根目录下的文件，则返回默认的 Cloudflare Pages 网页
-  if (path !== '/') {
-    return fetch(request)
+  // 获取 GitHub 仓库的目录和文件
+  const response = await fetch(`https://api.github.com/repos/wangchunin/SHELL/contents${path}`, {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (compatible; Cloudflare-Worker/1.0)',
+      'Accept': 'application/vnd.github.v3+json'
+    }
+  })
+
+  if (!response.ok) {
+    return new Response('Not found', { status: 404 })
   }
 
-  const response = new Response()
-  const headers = { 'Content-Type': 'text/html' }
-  const files = await getFiles()
-
-  // 构建文件列表的 HTML
-  const fileList = files.map(file => `<li><a href="/${file}">${file}</a></li>`).join('')
-  const html = `
-    <html>
-      <head>
-        <title>文件列表</title>
-      </head>
-      <body>
-        <h1>文件列表</h1>
-        <ul>${fileList}</ul>
-      </body>
-    </html>
-  `
-
-  response.headers.set('Content-Type', 'text/html')
-  response.body = html
-
-  return response
-}
-
-async function getFiles() {
-  // 使用 GitHub API 获取仓库根目录下的文件列表
-  const response = await fetch('https://api.github.com/repos/wangchunin/SHELL/contents')
   const data = await response.json()
 
-  // 过滤出文件，并返回文件名列表
-  const files = data.filter(item => item.type === 'file').map(item => item.name)
-  return files
+  if (Array.isArray(data)) {
+    // 如果返回的是目录列表，则生成目录的 HTML 页面
+    const html = generateDirectoryPage(data, path)
+    return new Response(html, { headers: { 'Content-Type': 'text/html' } })
+  } else {
+    // 如果返回的是文件内容，则直接跳转到 GitHub 的文件显示页面
+    const fileUrl = data.html_url
+    return Response.redirect(fileUrl, 302)
+  }
+}
+
+function generateDirectoryPage(data, currentPath) {
+  // 生成目录的 HTML 页面
+  let html = '<h1>Directory</h1>'
+  html += '<ul>'
+  data.forEach(item => {
+    const itemName = item.name
+    const itemPath = item.path
+    const isDirectory = item.type === 'dir'
+    const link = isDirectory ? `<a href="${itemPath}">${itemName}</a>` : `<a href="${item.html_url}" target="_blank">${itemName}</a>`
+    html += `<li>${link}</li>`
+  })
+  html += '</ul>'
+  return html
 }
